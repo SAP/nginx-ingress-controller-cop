b3a4e2cbcc58dbcec694edf68543bfd0255400eb
Christoph Barbian <christoph.barbian@sap.com>
2026-01-04 15:52:36 +0100

    update mods

--- hack/tools.go
+++ hack/tools.go
@@ -2,7 +2,7 @@
 // +build tools
 
 /*
-SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and nginx-ingress-controller-cop contributors
+SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and nginx-ingress-controller-cop contributors
 SPDX-License-Identifier: Apache-2.0
 */
 
